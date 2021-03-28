932ead3ea05af080e274388f3f7165040700dec8
Ontorok <nasir.amath07@gmail.com>
2021-03-28 17:20:16 +0600

    redux setup

---
+++ src/redux/reducers/ingredient.js
@@ -0,0 +1,12 @@
+import * as actionName from '../actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4,
+}
+
+const ingredient = (state = initialState, action) => {
+    return state;
+}
+
+export default ingredient
